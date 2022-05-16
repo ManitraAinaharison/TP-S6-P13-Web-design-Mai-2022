cdee55ef02b22ccfadfd575fd81c0efb4a4d6dbf
ManitraAinaharison <manitraainaharison@gmail.com>
2022-05-16 10:47:50 +0300

    reglage des configurations

---
+++ application/helpers/assets_helper.php
@@ -0,0 +1,28 @@
+<?php 
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+if ( ! function_exists('css_url')) {
+	function css_url($file) {
+		return site_url("assets/css/".$file);
+	}
+}
+
+if ( ! function_exists('js_url')) {
+	function js_url($file) {
+		return site_url("assets/js/".$file);
+	}
+}
+
+if ( ! function_exists('img_url')) {
+	function img_url($file) {
+		return site_url("assets/img/".$file);
+	}
+}
+
+if ( ! function_exists('lib_url')) {
+	function lib_url($file) {
+		return site_url("assets/lib/".$file);
+	}
+}
+
+?>
